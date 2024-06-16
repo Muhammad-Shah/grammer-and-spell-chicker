453ffd956650292c80b1bb6712c98bfeea4cb492
Muhammad-Shah <msh87376@gmail.com>
2024-06-16 20:08:52 +0500

    files added

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import streamlit as st
 import language_tool_python
 
-tool = language_tool_python.LanguageTool('en-US')  # use a local server
-tool = language_tool_python.LanguageToolPublicAPI('en-US') # or use public API
+# tool = language_tool_python.LanguageTool('en-US')  # use a local server
+tool = language_tool_python.LanguageToolPublicAPI('en-US')  # or use public API
+
 
 def text_correction(text):
-    return tool(text)
+    return tool.correct(text=text)
+
 
 def main():
-    st.header('Spelling and Grammer Correction App')
+    st.header('Spelling and Grammar Correction App')
     text_area = st.text_area('Enter your text...')
     if text_area is not None:
-         if st.button('Submit'):
+        if st.button('Submit'):
             st.write('Input text is: ')
             st.success(text_area)
             corrected_text = text_correction(text_area)
             st.write(corrected_text)
+
 
 if __name__ == '__main__':
     main()
